d1dac5254c6b97bae8e3afeff1b9d599c7d873fb
DubleRay <ray.doronichev@gmail.com>
2022-06-10 14:39:28 +0300

    Delit trash

--- JB.js
+++
@@ -1,9 +0,0 @@
-const Diamonds = [2,3,4,5,6,7,8,9,10,10,10,10,11];
-const Hearts = [2,3,4,5,6,7,8,9,10,10,10,10,11];
-const Spades = [2,3,4,5,6,7,8,9,10,10,10,10,11];
-const Clubs = [2,3,4,5,6,7,8,9,10,10,10,10,11];
-function getcard(){
-    var y = Diamonds.sort(() => Math.random()-0.5)
-    var x = y[0];
- y.pop();
-return x }
